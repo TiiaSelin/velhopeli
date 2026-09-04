afa252ce9a73ebec04367336e582732585ead8f7
ExcavatorZ <excavatorz@hotmail.fi>
2026-09-04 22:14:59 +0300

    Lisätty elämäpalkki, vahingonotto ja pelin loppuminen.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,9 +2,6 @@ extends CharacterBody2D
 
 var health = 100.0
 
-func _ready() -> void:
-	%HealthBar.value = health
-
 # Pelaajahahmon liike
 func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -12,5 +9,13 @@ func _physics_process(_delta: float) -> void:
 	velocity = direction * 650
 	move_and_slide()
 
-	if health <= 0.0:
-		print_debug("Game over.")
+	# Player damage and game over.
+	const DAMAGE_RATE = 50.0
+	var overlapping_enemies = %HurtBox.get_overlapping_bodies()
+	
+	if overlapping_enemies.size() > 0:
+		health -= DAMAGE_RATE * overlapping_enemies.size() * _delta
+		%HealthBar.value = health
+		if health <= 0.0:
+			print("Game over.")
+			get_tree().paused = true
