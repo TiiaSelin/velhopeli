32b960bc12e0eba10c8ff53af94e3d363e3930d2
TiiaSelin <tiiaselin92@gmail.com>
2026-09-04 20:50:52 +0300

    vihollisen liikemekaniikka

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 # Pelaajahahmon liike
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 #Hahmon nopeus
 	velocity = direction * 650
