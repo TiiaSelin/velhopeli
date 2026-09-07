ce313b5ce1ec7da39d407c91687cf0e605c12db4
ExcavatorZ <excavatorz@hotmail.fi>
2026-09-07 08:17:53 +0300

    Lisätty viholliselle health bar ja kuoleminen.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,9 +2,19 @@ extends CharacterBody2D
 
 @onready var player = get_node("/root/Game/Player")
 
+var health = 3
+
 #Vihollisen liike
 func _physics_process(_delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	
 	velocity = direction * 300.0
 	move_and_slide()
+
+# Damage and death. 
+func take_damage():
+	health -= 1
+	%HealthBar.value = health
+	
+	if health <= 0:
+		queue_free()
