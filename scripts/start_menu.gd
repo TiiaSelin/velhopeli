4b0fe0da7b172f071d0505d69f835c251e393491
ExcavatorZ <excavatorz@hotmail.fi>
2026-09-08 03:23:35 +0300

    Korjattu hahmovalikkoa ja tehty sille toimiva runko.

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,7 +1,9 @@
 extends Control
 
+@onready var character_selection = preload("res://scenes/character_selection.tscn")
+
 func _on_character_selection_pressed() -> void:
-	print("Character selection pressed.")
+	get_tree().change_scene_to_packed(character_selection)
 
 func _on_controls_pressed() -> void:
 	print("Controls pressed.")
