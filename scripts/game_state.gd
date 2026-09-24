78f6cc40f7ec2bd389cbc043ae3ea41e0ba570f9
ExcavatorZ <excavatorz@hotmail.fi>
2026-09-24 05:11:23 +0300

    Siirrytty pelaajan logiikkaa pelin tilaan.

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -1,10 +1,13 @@
 extends Node
 
+var health_potion = preload("res://resources/items/health_potion.tres")
+var mana_potion = preload("res://resources/items/mana_potion.tres")
+
 var currency = 10
 var elements = {}
 var forms = {}
 var effects = {}
-var items = {}
+var inventory: Inventory = Inventory.new()
 
 var default_spell = {
 	"element": "None",
@@ -23,3 +26,7 @@ var selected_spell = {
 	"effect": null,
 	"effect_power": 1
 }
+
+func _ready() -> void:
+	inventory.add_item(health_potion)
+	inventory.add_item(mana_potion)
